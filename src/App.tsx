966c8fce004f36274945ab9e1b82eff200b5c235
Rafael <rafael-srv@hotmail.com>
2023-03-21 15:09:14 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as C from './App.styles'
+import { Character } from './components/Character'
+import { useCharacter } from './hooks/useCharacter'
+
+export const App = ()=>{
+  const char = useCharacter();
+  
+  return(
+    <C.Container>
+      <C.Map>
+        <Character x={char.x} y={char.y}/>
+      </C.Map>
+    </C.Container>
+
+  )
+
+}
+
+export default App
