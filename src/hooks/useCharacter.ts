18a16069a3e10249c977255797e9538d05eabaeb
Rafael <rafael-srv@hotmail.com>
2023-04-24 00:23:07 -0300

    Novo Personagem

--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -8,7 +8,7 @@ type UseCharacterProps = {
     coord: {
     x: number,
     y: number}
-    } // Props novo dchar...!!dl326daskddSldg34glçocdw4
+    } // Props novo dchar...!!dl326daskddSldg34glrfgedçocdw4
 
 export const useCharacter = ( {propName, coord} : UseCharacterProps)=>{
     
